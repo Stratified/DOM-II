06ed3a84653e5c73243729189c99a9037833f783
Brian Bagrov <Brianbagrov@gmail.com>
2020-06-04 21:45:56 -0400

    Completed three event listeners.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+const headerImg = document.querySelector("img");
+headerImg.addEventListener("mousedown", () => {
+  headerImg.style.transform = "scale(1.2)";
+});
+
+headerImg.addEventListener("mouseup", () => {
+  headerImg.style.transform = "scale(1)";
+});
+
+const body = document.querySelector("body");
+body.addEventListener("keydown", () => {
+  body.style.color = "orange";
+});
