946d7786ba9d4e61eaf1ffbdf96e522c1a061585
Brian Bagrov <Brianbagrov@gmail.com>
2020-06-05 20:49:55 -0400

    I am done.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,67 @@
-const headerImg = document.querySelector("img");
-headerImg.addEventListener("mousedown", () => {
-  headerImg.style.transform = "scale(1.2)";
+const title = document.querySelector(".logo-heading");
+title.addEventListener("click", () => {
+  title.style.color = "green";
 });
 
-headerImg.addEventListener("mouseup", () => {
-  headerImg.style.transform = "scale(1)";
+const navBar = document.querySelectorAll("a");
+navBar.forEach((nav) => {
+  nav.addEventListener("click", (e) => {
+    e.target.style.color = "purple";
+  });
+});
+
+const imgs = document.querySelectorAll("img");
+imgs.forEach((img) => {
+  img.addEventListener("mousedown", (e) => {
+    e.target.style.transform = "scale(1.2)";
+  });
+});
+imgs.forEach((img) => {
+  img.addEventListener("mouseup", (e) => {
+    e.target.style.transform = "scale(1)";
+  });
+});
+
+imgs.forEach((img) => {
+  img.addEventListener("mouseenter", (e) => {
+    e.target.style.transform = "scale(1.1)";
+  });
+});
+imgs.forEach((img) => {
+  img.addEventListener("mouseleave", (e) => {
+    e.target.style.transform = "scale(1)";
+  });
 });
 
 const body = document.querySelector("body");
 body.addEventListener("keydown", () => {
   body.style.color = "orange";
+});
+
+const para = document.querySelectorAll("p");
+para.forEach((p) => {
+  p.addEventListener("mouseover", (e) => {
+    e.target.style.color = "purple";
+  });
+  p.addEventListener("mouseleave", (e) => {
+    e.target.style.color = "black";
+  });
+});
+
+const button = document.querySelectorAll(".btn");
+button.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.target.style.fontSize = "3rem";
+    e.target.style.transition = "ease-in-out 1s";
+  });
+});
+
+const h2 = document.querySelectorAll("h2");
+h2.forEach((h) => {
+  h.addEventListener("click", (e) => {
+    e.target.style.color = "red";
+  });
+});
+h2.addEventListener("dblclick", () => {
+  h2.style.color = "blue";
 });
